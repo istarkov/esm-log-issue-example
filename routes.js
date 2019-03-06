5cae0a25cba1d39b8bd067a35114ce425c91c28b
Ivan Starkov <istarkov@gmail.com>
2019-03-06 18:17:28 +0300

    Update routes

--- routes.js
+++ routes.js
@@ -1,17 +1,18 @@
-// THIS IMPORT BREAKS log of 2,3
+// THIS IMPORT BREAKS log of 2,3, 3
+// without it all is logged fine
 import { parse } from 'url';
 
 export const routes = (req, res) => {
-  // log 1
+  // log 1 - works always
   console.log('HELLO');
 
-  // log 2
+  // log 2 - works only if there is no imports in this file
   console.log({ a: 1 });
 
-  // log 3
+  // log 3 - works only if there is no imports in this file
   console.log(JSON.stringify({ a: 1 }, null, ' '));
 
-  // log 4
+  // log 4 - works only if there is no imports in this file
   console.log([1]);
 
   res.end('Im working');
